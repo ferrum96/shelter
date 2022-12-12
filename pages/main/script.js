be49d2d00a4d1d2187895ad45073d05aa5508bd1
Константин <51968997+ferrum96@users.noreply.github.com>
2022-12-13 02:49:03 +0500

    Added modal window, fixed_header. Refactored slider

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,31 +1,84 @@
+// Fixed header
 
+const header = document.querySelector('.header'),
+	headerHeight = header.offsetHeight;
 
-// Slider 
+window.addEventListener('scroll', () => {
+	let scrollDistance = window.scrollY;
+	console.log(scrollDistance)
 
-const buttonPrev = document.querySelector('.slider__button_prev');
-const buttonNext = document.querySelector('.slider__button_next');
-const sliderBox = document.querySelector('.slider__items');
-const sliderItems = document.getElementsByClassName('pet__card');
-const visibleItemsCount = 3;
-const gap = 90;
-const itemWidth = sliderItems[0].offsetWidth + gap;
-const itemsWidth = itemWidth * sliderItems.length;
-let left = 0;
-
-buttonPrev.addEventListener('click', () => {
-	const sliderBox = document.querySelector('.slider__items');
-	if(sliderItems.length > visibleItemsCount && left < 0) {
-		left = left + itemWidth;
+	if (scrollDistance >= headerHeight) {
+		header.classList.add('header_fixed');
+	} else {
+		header.classList.remove('header_fixed');
 	}
-	sliderBox.style.left = left + 'px';
 });
 
-buttonNext.addEventListener('click', () => {
-	const sliderBox = document.querySelector('.slider__items');
-	if(sliderItems.length > visibleItemsCount && left > (-itemsWidth + itemWidth * visibleItemsCount)) {
-		left = left - itemWidth;
-	} else {
-		left = 0;
-	}
-	sliderBox.style.left = left + 'px';
+// Slider
+let position = 0;
+const slidesToShow = 3;
+const slidesToScroll = 1;
+const sliderContainer = document.querySelector('.slider__container');
+const sliderTrack = document.querySelector('.slider__track');
+const sliderItems = document.querySelectorAll('.slider__item');
+const itemsCount = sliderItems.length;
+const buttonPrev = document.querySelector('.slider__button_prev');
+const buttonNext = document.querySelector('.slider__button_next');
+let gap = 90;
+const itemWidth = (sliderContainer.clientWidth + gap) / slidesToShow;
+console.log(sliderContainer.clientWidth)
+const movePosition = slidesToScroll * itemWidth;
+
+sliderItems.forEach((item) => {
+	item.style.minWidth = `${itemWidth}px`
+})
+
+buttonNext.addEventListener('click', () =>{
+	const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+	position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+
+	setPosition();
+	checkBtns();
+});
+
+buttonPrev.addEventListener('click', () =>{
+	const itemsLeft = Math.abs(position) / itemWidth;
+	position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+
+	setPosition();
+	checkBtns();
+});
+
+const setPosition = () => {
+	sliderTrack.style.transform = `translateX(${position}px)`;
+};
+
+const checkBtns = () => {
+	buttonPrev.disabled = position === 0;
+	buttonNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+};
+
+// Modal window
+
+const petButtons = document.querySelectorAll('.pet__button');
+const modalCloseButtons = document.querySelectorAll('.modal__button_close');
+const modalOverlay = document.querySelector('.modal__overlay');
+const modals = document.querySelectorAll('.modal');
+
+petButtons.forEach((el) => {
+	el.addEventListener('click', (e) => {
+		let path = e.currentTarget.getAttribute('data-path');
+
+		document.querySelector(`[data-target="${path}"]`).classList.add('modal_visible');
+		modalOverlay.classList.add('modal__overlay_visible');
+	});
+});
+
+modalCloseButtons.forEach((el) => {
+	el.addEventListener('click', (e) => {
+		modals.forEach((el) => {
+			el.classList.remove('modal_visible');
+		})
+		modalOverlay.classList.remove('modal__overlay_visible');
+	});
 });
