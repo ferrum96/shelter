bb5bba825e2a16964eb57109b5d4d0e064073d42
Константин <51968997+ferrum96@users.noreply.github.com>
2022-12-18 21:56:34 +0500

    Added disabled button style Moved pet card and modal window styles to base.css Added responsible styles for main page slider

--- pages/main/script.js
+++ pages/main/script.js
@@ -14,7 +14,9 @@ window.addEventListener('scroll', () => {
 });
 
 // Slider
-var swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".swiper", {
+	speed: 600,
+	allowTouchMove: false,
 	spaceBetween: 30,
 	loop: true,
 	onlyExternal: true,
@@ -41,28 +43,4 @@ var swiper = new Swiper(".swiper", {
 			spaceBetween: 40,
 		},
 	}
-});
-
-// Modal window
-const petButtons = document.querySelectorAll('.pet__button');
-const modalCloseButtons = document.querySelectorAll('.modal__button_close');
-const modalOverlay = document.querySelector('.modal__overlay');
-const modals = document.querySelectorAll('.modal');
-
-petButtons.forEach((el) => {
-	el.addEventListener('click', (e) => {
-		let path = e.currentTarget.getAttribute('data-path');
-
-		document.querySelector(`[data-target="${path}"]`).classList.add('modal_visible');
-		modalOverlay.classList.add('modal__overlay_visible');
-	});
-});
-
-modalCloseButtons.forEach((el) => {
-	el.addEventListener('click', (e) => {
-		modals.forEach((el) => {
-			el.classList.remove('modal_visible');
-		})
-		modalOverlay.classList.remove('modal__overlay_visible');
-	});
 });
