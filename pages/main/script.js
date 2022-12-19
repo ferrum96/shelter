ce7dfc1674ca39ee62483ada09eea645582df336
Константин <51968997+ferrum96@users.noreply.github.com>
2022-12-20 00:47:48 +0500

    Deleted normilize.css Fixed header and help block in main page

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,10 +1,14 @@
 // Fixed header
 const header = document.querySelector('.header'),
-	headerHeight = header.offsetHeight;
+	headerHeight = header.offsetHeight,
+	startScreen = document.querySelector('.start-screen'),
+	anhors = [document.querySelector("a[href='#about-the-shelter']"),
+	document.querySelector("a[href='#help-the-shelter']"),
+	document.querySelector("a[href='#contacts']"),
+	document.querySelector("a[href='#make-a-friend']")];
 
 window.addEventListener('scroll', () => {
 	let scrollDistance = window.scrollY;
-	// console.log(scrollDistance)
 
 	if (scrollDistance >= headerHeight) {
 		header.classList.add('header_fixed');
@@ -12,6 +16,22 @@ window.addEventListener('scroll', () => {
 		header.classList.remove('header_fixed');
 	}
 });
+
+for (let anhor of anhors) {
+	anhor.addEventListener('click', (event) => {
+		event.preventDefault();
+		const blockId = anhor.getAttribute('href');
+		const scrollTarget = document.querySelector('' + blockId);
+		const topOffset = 100;
+		const elementPosition = scrollTarget.getBoundingClientRect().top;
+		const offsetPosition = elementPosition - topOffset;
+
+		window.scrollBy({
+			top: offsetPosition,
+			behavior: 'smooth'
+		});
+	});
+}
 
 // Slider
 const swiper = new Swiper(".swiper", {
